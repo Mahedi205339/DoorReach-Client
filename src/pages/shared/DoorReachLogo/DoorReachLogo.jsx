e9c17e36510183379050d7cb757455d72a86e271
Mahedi205339 <mahedi205339@gmail.com>
2025-10-29 03:36:46 +0600

    added custom font

--- src/pages/shared/DoorReachLogo/DoorReachLogo.jsx
+++ src/pages/shared/DoorReachLogo/DoorReachLogo.jsx
@@ -5,7 +5,7 @@ const DoorReachLogo = () => {
     return (
         <div className='flex items-end'>
                 <img src={logo} alt="" />
-            <p className='text-2xl font-semibold bg-gradient-to-r from-orange-700 to-orange-500 bg-clip-text text-transparent'>Door<span className='italic text-3xl '>Reach</span> </p>
+            <p className='text-2xl font-semibold bg-gradient-to-r from-orange-700 to-orange-500 bg-clip-text text-transparent'>Door<span className='italic font-extrabold text-3xl '>Reach</span> </p>
         </div>
     )
 }

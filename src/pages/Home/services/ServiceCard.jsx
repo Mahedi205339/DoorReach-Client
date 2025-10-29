87b11db901092c3badde698ec0a45f0d76d95361
Mahedi205339 <mahedi205339@gmail.com>
2025-10-30 01:37:11 +0600

    added our services and service card

--- src/pages/Home/services/ServiceCard.jsx
+++ src/pages/Home/services/ServiceCard.jsx
@@ -6,11 +6,11 @@ const ServiceCard = ({ service }) => {
     const { title, description, icon: Icon } = service;
 
     return (
-        <div className="card bg-base-100 shadow-md hover:shadow-lg transition-all duration-300 p-6 text-center rounded-xl">
+        <div className="card bg-base-100 shadow-md hover:shadow-lg transition-all duration-300 p-6 text-center rounded-xl hover:bg-orange-100">
             <div className="flex justify-center mb-4 text-orange-400 text-4xl lg:text-5xl xl:text-5xl">
                 <Icon />
             </div>
-            <h3 className="text-lg  font-semibold bg-gradient-to-r from-orange-700 to-yellow-500 bg-clip-text text-transparent mb-2">{title}</h3>
+            <h3 className="text-lg  font-semibold bg-gradient-to-r from-orange-600 to-orange-400 bg-clip-text text-transparent mb-2">{title}</h3>
             <p className="text-sm text-gray-600">{description}</p>
         </div>
     );
